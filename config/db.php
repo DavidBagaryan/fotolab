8f6ecae08653778539c037a555c850ba8b3461fe
David Bagarian <david.bagaryan@mail.ru>
2018-07-15 22:51:47 +0300

    init commit for fotolab

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=fotolab',
+    'username' => 'fotolab-user',
+    'password' => '2p9sY4C9CDSuf9hW',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
